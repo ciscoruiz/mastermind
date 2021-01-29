d285106e8b3bee958d894fda576aa50ccafa2980
Francisco Ruiz <cisco.tierra@gmail.com>
2021-01-29 08:22:58 +0000

    Remove class ConfigurationView and unused classes

--- src/com/upm/master/mastermind/view/ConfigurationView.java
+++
@@ -1,9 +0,0 @@
-package com.upm.master.mastermind.view;
-
-import com.upm.master.mastermind.model.Configuration;
-
-public interface ConfigurationView {
-   public void setMaxAttempt(Configuration configuration);
-   public void setMaxPlay(Configuration configuration);
-   public void setValidFigures(Configuration configuration);
-}
