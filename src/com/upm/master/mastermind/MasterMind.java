4d9aea912e3a267b9dd0a98d967c12a0aca704b9
Francisco Ruiz <cisco.tierra@gmail.com>
2021-02-15 11:38:36 +0000

    Rename Main.java as MasterMind.java

--- src/com/upm/master/mastermind/MasterMind.java
+++ src/com/upm/master/mastermind/MasterMind.java
@@ -7,7 +7,7 @@ import com.upm.master.mastermind.model.ValidFigures;
 import com.upm.master.mastermind.view.console.GameConsole;
 import com.upm.master.mastermind.view.console.MasterMindConsole;
 
-public class Main {
+public class MasterMind {
     public static void main(String[] args) {
        Configuration configuration = createHardcodedConfiguration();
        GameController gameController = new GameController(new GameConsole());
